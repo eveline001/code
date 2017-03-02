869388cf3496a84b978ef2832653318ffaedc430
eveline001 <eveline001@yeah.net>
2017-03-02 18:34:04 +0800

    accepted

--- hdu/6015/main.cpp
+++ hdu/6015/main.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <vector>
 #include <map>
 #include <cstdio>
 
 using namespace std;
 
-map<string, vector<int>> RE;
+struct Node{
+    int a, b;
+
+    Node(): a(0), b(0) {}
+
+    void Push(int x){
+        if(x > a){
+            b = a;
+            a = x;
+        }else if(x > b){
+            b = x;
+        }
+    }
+
+    int Sum(){
+        return a + b;
+    }
+};
+
+map<string, Node> RE;
 
 int main()
 {
-    int T, n, val;
-    char* str;
+    int T, n, val, ans;
+    char str[16];
 
     scanf("%d", &T);
     for(int cas = 1; cas <= T; cas++){
         scanf("%d" , &n);
+        RE.clear();
         for(int i = 0; i < n; i++){
             scanf("%s %d", str, &val);
             string name(str);
-            RE[name].push_back(val);
+            RE[name].Push(val);
         }
+        ans = 0;
         for(auto& it : RE){
-            sort(it.second.begin(), it.second.end());
+            ans += it.second.Sum();
         }
+        printf("%d\n", ans);
     }
+    return 0;
 }
