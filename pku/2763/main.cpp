5625a4c54f84366f9c0fe5cc7c8acceb43a9eb46
eveline001 <eveline001@yeah.net>
2017-03-21 12:02:20 +0800

    accepted

--- pku/2763/main.cpp
+++ pku/2763/main.cpp
@@ -1,178 +1,4 @@
-/*
-*
-http://blog.csdn.net/y990041769/article/details/40983749 2125ms
-TLE
-*/
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-#define Del(a,b) memset(a,b,sizeof(a))
-
-const int N = 100005;
-int dep[N],siz[N],fa[N],id[N],son[N],val[N],top[N]; //top ×î½üµÄÖØÁ´¸¸½Úµã
-int num,head[N],cnt;
-
-struct Edge{
-    int to,next;
-};
-
-Edge v[N*2];
-
-struct tree{
-    int x, y, val;
-    void read(){
-        scanf("%d%d%d", &x, &y, &val);
-    }
-};
-
-void add_Node(int x,int y)
-{
-    v[cnt].to = y;
-    v[cnt].next = head[x];
-    head[x] = cnt++;
-}
-
-tree e[N];
-
-void dfs1(int u, int f, int d){
-    dep[u] = d, siz[u] = 1, son[u] = 0, fa[u] = f;
-    for(int i = head[u]; i != -1 ;i = v[i].next){
-        int to = v[i].to;
-        if(to != f){
-            dfs1(to,u,d+1);
-            siz[u] += siz[to];
-            if(siz[son[u]] < siz[to])
-                son[u] = to;
-        }
-    }
-}
-
-void dfs2(int u, int tp){
-    top[u] = tp;
-    id[u] = ++num;
-    if (son[u]) dfs2(son[u], tp);
-    for(int i = head[u]; i != -1 ;i = v[i].next)
-    {
-        int to = v[i].to;
-        if(to == fa[u] || to == son[u])
-            continue;
-        dfs2(to,to);
-    }
-}
-
-#define lson(x) ((x<<1))
-#define rson(x) ((x<<1)+1)
-
-struct Tree
-{
-    int l,r,val;
-};
-
-Tree tree[4*N];
-
-void pushup(int x){
-    tree[x].val = tree[lson(x)].val + tree[rson(x)].val;
-}
-
-void build(int l,int r,int v)
-{
-    tree[v].l=l;
-    tree[v].r=r;
-    if(l==r){
-        tree[v].val = val[l];
-        return;
-    }
-    int mid=(l+r)>>1;
-    build(l,mid,v*2);
-    build(mid+1,r,v*2+1);
-    pushup(v);
-}
-
-void update(int o,int v,int val)  //log(n)
-{
-    if(tree[o].l==tree[o].r)
-    {
-        tree[o].val = val;
-        return ;
-    }
-    int mid = (tree[o].l+tree[o].r)/2;
-    if(v<=mid)
-        update(o*2,v,val);
-    else
-        update(o*2+1,v,val);
-    pushup(o);
-}
-
-int query(int o,int l, int r)
-{
-    if(tree[o].l >= l && tree[o].r <= r){
-        return tree[o].val;
-    }
-    int mid = (tree[o].l + tree[o].r) / 2;
-    if(r<=mid)
-        return query(o+o,l,r);
-    else if(l>mid)
-        return query(o+o+1,l,r);
-    else
-        return query(o+o,l,mid) + query(o+o+1,mid+1,r);
-}
-
-int Yougth(int u, int v){
-    int tp1 = top[u], tp2 = top[v];
-    int ans = 0;
-    while (tp1 != tp2){
-        if (dep[tp1] < dep[tp2]){
-            swap(tp1, tp2);
-            swap(u, v);
-        }
-        ans += query(1,id[tp1], id[u]);
-        u = fa[tp1];
-        tp1 = top[u];
-    }
-    if(u == v) return ans;
-    if(dep[u] > dep[v]) swap(u, v);
-    ans += query(1,id[son[u]], id[v]);
-    return ans;
-}
-int main()
-{
-    int n, m, s;
-    while(scanf("%d%d%d", &n, &m, &s) != EOF){
-        cnt = 0;
-        memset(head, -1, sizeof(head));
-        for(int i = 1; i < n; i++){
-            e[i].read();
-            add_Node(e[i].x, e[i].y);
-            add_Node(e[i].y, e[i].x);
-        }
-        num = 0;
-        dfs1(1, 0, 1);
-        dfs2(1, 1);
-        for(int i = 1; i < n; i++){
-            if(dep[e[i].x] < dep[e[i].y]) swap(e[i].x, e[i].y);
-            val[id[e[i].x]] = e[i].val;
-        }
-        build(1, num, 1);
-        for(int i = 0; i < m; i++){
-            int ok, x, y;
-            scanf("%d", &ok);
-            if(ok == 0){
-                scanf("%d", &x);
-                printf("%d\n", Yougth(s, x));
-                s = x;
-            }else{
-                scanf("%d%d", &x, &y);
-                update(1,id[e[x].x],y);
-            }
-        }
-    }
-    return 0;
-}
-/*
+// é€’å½’TLEæˆ–è€…RE, è¿­ä»£å°±èƒ½è¿‡...
 #include <iostream>
 #include <algorithm>
 #include <cstdio>
@@ -181,6 +7,8 @@ int main()
 using namespace std;
 
 const int MAX = 100001;
+
+int n;
 
 struct Edge{
     int to, next;
@@ -271,27 +99,24 @@ int Query(int L, int R, int l, int r, int rt){
     return res;
 }
 
-int ChainQuery(int a, int b, int n){
-    if(a == b){
-        return 0;
-    }
-    if(tp[a] == tp[b]){
-        if(id[a] > id[b]){
-            return Query(id[b] + 1, id[a], 2, n, 1);
-        }else{
-            return Query(id[a] + 1, id[b], 2, n, 1);
+int ChainQuery(int a, int b){
+    int tp1 = tp[a], tp2 = tp[b], res = 0;
+    while(tp1 != tp2){
+        if(de[tp1] < de[tp2]){
+            swap(tp1, tp2), swap(a, b);
         }
+        res += Query(id[tp1], id[a], 1, n, 1);
+        a = fa[tp1];
+        tp1 = tp[a];
     }
-    if(tp[a] < tp[b]){
-        return Query(id[tp[b]], id[b], 2, n, 1) + ChainQuery(a, fa[b], n);
-    }else{
-        return Query(id[tp[a]], id[a], 2, n, 1) + ChainQuery(fa[a], b, n);
-    }
+    if(a == b) return res;
+    if(id[a] > id[b]) swap(a, b);
+    return res + Query(id[a] + 1, id[b], 1, n, 1);
 }
 
 int main()
 {
-    int n, q, s, u, v, w, op;
+    int q, s, u, v, w, op;
 
     while(scanf("%d%d%d", &n, &q, &s) != EOF){
         memset(head, -1, sizeof(head));
@@ -311,19 +136,18 @@ int main()
             }
             val[id[Road[i][0]]] = Road[i][2];
         }
-        BuildUp(2, n, 1);
+        BuildUp(1, n, 1);
         for(int i = 0; i < q; i++){
             scanf("%d", &op);
             if(op == 0){
                 scanf("%d", &u);
-                printf("%d\n", ChainQuery(s, u, n));
+                printf("%d\n", ChainQuery(s, u));
                 s = u;
             }else if(op == 1){
                 scanf("%d%d", &v, &w);
-                Update(id[Road[v][0]], w, 2, n, 1);
+                Update(id[Road[v][0]], w, 1, n, 1);
             }
         }
     }
     return 0;
 }
-*/
