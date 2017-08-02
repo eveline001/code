ae2be37d44d66d5fb87fb91ca0f8c1ef423281d3
eveline001 <eveline001@yeah.net>
2017-08-02 17:22:53 +0800

    init

--- hdu/1074/main.cpp
+++ hdu/1074/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdio>
 
 using namespace std;
@@ -17,17 +18,28 @@ int d[20], c[20];
 
 int main()
 {
-    int T;
+    int T, endsta, presta, temp;
 
     scanf("%d", &T);
     for(int cas = 1; cas <= T; cas++){
         scanf("%d", &n);
         for(int i = 0; i < n; i++) scanf("%s%d%d", s[i], &d[i], &c[i]);
-        for(int sta = 1; sta < (1 << (n + 1)) - 1; sta++){
+        endsta = (1 << (n + 1)) - 1, dp[1 << i].toltime = 0, dp[1 << i].presta = -1, dp[1 << i].lost = 0;
+        for(int sta = 1; sta < endsta; sta++){
             dp[sta].lost = INF;
-            for(int i = n - 1; i >= 0; i--){
-
+            for(int i = 0; i < n; i++){
+                if(sta & (1 << i) == 1){
+                    presta = sta - (1 << i);
+                    temp = max(0, dp[presta].toltime + c[i] - d[i]);
+                    if(dp[sta].lost > temp){
+                        dp[sta].toltime = dp[presta].toltime + c[i];
+                        dp[sta].presta = i;
+                        dp[sta].lost = temp;
+                    }
+                }
             }
         }
+        printf("%d\n", dp[endsta].lost);
+        for(int i = dp[endsta].presta; ~i; i = )
     }
 }
