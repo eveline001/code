5d0b2a3201aa24b957d30ed703ecfb6e9994c40b
eveline001 <eveline001@yeah.net>
2017-08-27 09:52:54 +0800

    init

--- hdu/4734/main.cpp
+++ hdu/4734/main.cpp
@@ -4,14 +4,28 @@
 
 using namespace std;
 
-int dp[10][4600];
-int Dig[10];
+int dp[10][4600], Dig[10], tol;
+
+int f(int x){
+    if(x == 0) return 0;
+    int res = f(x / 10);
+    return res * 2 + x % 10;
+}
+
+int DigtialDFS(int pos, int sum, bool limit){
+    if(pos == -1) return sum <= tol;
+    if(sum > tol) return 0;
+    if(!limit && dp[pos][tol - sum] != -1) return dp[pos][tol - sum];
+    int top = limit ? Dig[pos] : 9, res = 0;
+    for(int i = 0; i <= top; i++) res += DigtialDFS(pos - 1, sum + i * (1 << pos), limit && i == Dig[pos]);
+    if(!limit) dp[pos][tol - sum] = res;
+    return res;
+}
 
 int DigtialDP(int x, int A){
     int pos = 0;
     while(x) Dig[pos++] = x % 10, x /= 10;
-
-    return DigtialDFS(pos - 1, );
+    return DigtialDFS(pos - 1, 0, true);
 }
 
 int main()
@@ -22,6 +36,8 @@ int main()
     scanf("%d", &T);
     for(int cas = 1; cas <= T; cas++){
         scanf("%d%d", &A, &B);
+        tol = f(A);
         printf("Case #%d: %d\n", cas, DigtialDP(B, A));
     }
+    return 0;
 }
